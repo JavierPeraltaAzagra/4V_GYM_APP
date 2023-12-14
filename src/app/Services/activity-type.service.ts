7500c37831dab1b125853c4918b3ac67ff133ec0
Javier <javierpe12345@gmail.com>
2023-12-14 10:33:39 +0100

    ActivityTypeService hecho

--- src/app/Services/activity-type.service.ts
+++ src/app/Services/activity-type.service.ts
@@ -4,6 +4,17 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class ActivityTypeService {
-
   constructor() { }
+  getActivities(): Activity_type[] {
+    let activity_type = [{ id: 1, name: "Spinning", number_monitors: 1 },
+    { id: 2, name: "BodyPump", number_monitors: 2 },
+    { id: 3, name: "Pilates", number_monitors: 1 }];
+    return activity_type;
+  }
+}
+
+export interface Activity_type {
+  id : Number;
+  name: String;
+  number_monitors: Number;
 }
