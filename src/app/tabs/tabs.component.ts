a980522b3a26b296bab1204da0c8c99dc6d1dae5
Javier <javierpe12345@gmail.com>
2023-12-19 11:12:36 +0100

    TabsComponent hecho

--- src/app/tabs/tabs.component.ts
+++ src/app/tabs/tabs.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-tabs',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './tabs.component.html',
   styleUrl: './tabs.component.scss'
 })
 export class TabsComponent {
+  selectedTab: number = 1;
 
+  setActiveTab(tabNumber: number): void {
+    this.selectedTab = tabNumber;
+  }
 }
